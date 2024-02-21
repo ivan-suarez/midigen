730b004ace1a62f9306f0306969d5b096eb3a8a4
ivan-suarez <ivansuarezcast@gmail.com>
2024-02-21 20:51:22 +0100

    Remove note generation

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-
 import pretty_midi
 import os
 
-midi_files = [f for f in os.listdir('/kaggle/input/classical-music-midi/bach/') if f.endswith('.mid')]
+midi_files = [f for f in os.listdir('bach/') if f.endswith('.mid')]
 
 midis = []
 for file in midi_files:
     try:
-        midi = pretty_midi.PrettyMIDI('/kaggle/input/classical-music-midi/bach/' + file)
+        midi = pretty_midi.PrettyMIDI('bach/' + file)
         midis.append(midi)
     except Exception as e:
         print(f"Error loading {file}: {e}")
@@ -77,69 +73,12 @@ model = Sequential([
 ])
 
 model.compile(optimizer='adam', loss='mean_squared_error')
+
+print(model.summary())
 # Fit the model to the training data
 history = model.fit(sequences, next_notes,
-                    epochs=100,  # Number of epochs to train for
-                    batch_size=64,  # Size of the batches of data
-                    verbose=1)  # Show training log
+                epochs=50,  # Number of epochs to train for
+                batch_size=64,  # Size of the batches of data
+                verbose=1)  # Show training log
 
-
-def generate_notes(model, seed_sequence, num_notes_to_generate):
-    generated_sequence = seed_sequence.copy()  # Copy the seed sequence
-    for _ in range(num_notes_to_generate):
-        # Reshape the sequence to match the model's input shape: (1, 50, 4)
-        input_sequence = np.array(generated_sequence[-50:]).reshape(1, 50, 4)
-        
-        # Predict the next note
-        predicted_note = model.predict(input_sequence)[0]  # model.predict returns a batch, so get the first
-        
-        # Append the predicted note to the sequence
-        generated_sequence.append(predicted_note.tolist())
-        
-    return generated_sequence
-
-# Assuming `x_train` is your training data and `seed_index` is some index of your choice
-seed_index = 0
-seed_sequence = sequences[seed_index][:50] # Starting with an existing sequence as seed
-num_notes_to_generate = 100  # Number of notes you want to generate
-
-# Generate notes
-generated_notes = generate_notes(model, seed_sequence, num_notes_to_generate)
-
-
-from mido import MidiFile, MidiTrack, Message
-
-def create_midi_from_notes(notes, output_file='generated_sequence.mid'):
-    midi = MidiFile()
-    track = MidiTrack()
-    midi.tracks.append(track)
-    
-    # Assuming the first event happens at time 0
-    last_event_time = 0
-    
-    for note in notes:
-        start_time, end_time, pitch, velocity = map(int, note)
-        
-        # Ensure non-negative delta times
-        delta_time_on = max(0, start_time - last_event_time)
-        delta_time_off = max(0, end_time - start_time)
-        
-        # Update last event time for the next iteration
-        # For note_on, this is the start time of the current note
-        last_event_time = start_time
-        
-        # Add the note_on message
-        track.append(Message('note_on', note=pitch, velocity=velocity, time=delta_time_on))
-        # For note_off, update the last event time to be the end time of the current note
-        last_event_time = end_time
-        
-        # Add the note_off message with time since the note_on
-        track.append(Message('note_off', note=pitch, velocity=0, time=delta_time_off))
-    
-    midi.save(output_file)
-
-output_path = '/kaggle/working/my_generated_midi.mid'
-create_midi_from_notes(generated_notes, output_file=output_path)
-
-
-
+model.save('models/bach.keras')
